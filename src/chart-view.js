a1fb30df69b8a9df038b57a2b612901f77c169ac
Ralf Sternberg <rsternberg@eclipsesource.com>
2019-02-19 23:23:54 +0100

    Adapt line chart to window width
    Auto-update width of line chart on window resize.

--- src/chart-view.js
+++ src/chart-view.js
@@ -4,30 +4,37 @@
   window.runtopsy.ChartView = { create };
 
   function create(id) {
-    let width = 700;
-    let height = 85;
-    let svg = d3.select(`#${id}`)
+    let container = document.getElementById(id);
+    let svg = d3.select(container)
       .append('svg:svg')
       .attr('width', '100%')
       .attr('height', '100%');
-    let x = d3.scaleLinear().range([0, width]);
-    let y = d3.scaleLinear().range([height, 0]);
-    var area = d3.area()
-      .x(d => x(d.distance))
-      .y0(height)
-      .y1(d => y(d.speed));
-    var line = d3.line()
-      .x(d => x(d.distance))
-      .y(d => y(d.speed));
+    let xRange = d3.scaleLinear();
+    let yRange = d3.scaleLinear();
+    let currentActivity;
+    window.addEventListener('resize', () => showActivity());
 
     return {
       showActivity
     };
 
-    function showActivity(activity) {
+    function showActivity(activity = currentActivity) {
+      if (!activity) return;
+      currentActivity = activity;
       let data = activity.records;
-      x.domain(d3.extent(data, d => d.distance));
-      y.domain([0, d3.max(data, d => d.speed)]);
+      let width = container.clientWidth;
+      let height = container.clientHeight;
+      xRange.range([0, width]);
+      yRange.range([height, 0]);
+      let area = d3.area()
+        .x(d => xRange(d.distance))
+        .y0(height)
+        .y1(d => yRange(d.speed));
+      let line = d3.line()
+        .x(d => xRange(d.distance))
+        .y(d => yRange(d.speed));
+      xRange.domain(d3.extent(data, d => d.distance));
+      yRange.domain([0, d3.max(data, d => d.speed)]);
       svg.selectAll('.line').remove();
       svg.selectAll('.area').remove();
       svg.append('path')
